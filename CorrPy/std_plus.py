91882bb023216a327dca53b7b723c5dc4bfae30a
K3ra-y <43222303+K3ra-y@users.noreply.github.com>
2019-02-12 12:54:08 -0800

    Update std_plus.py

--- CorrPy/std_plus.py
+++ CorrPy/std_plus.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def std_plus(x):
   '''
-  calculates the standard deviation of the values in a numeric vector. 
+  Calculates the standard deviation of the values in a numeric vector. 
   It is capable of computing standard deviation when the vector contains missing values 
   and inifinite values by automatically removing them.
 
