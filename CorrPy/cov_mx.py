76b68408814aefa26d0c7f376ad2a5698bec3a15
mru4913 <albertyu4913@gmail.com>
2019-02-12 12:07:36 -0800

    Update cov_mx and test_cov_mx

--- CorrPy/cov_mx.py
+++ CorrPy/cov_mx.py
@@ -1,31 +1,39 @@
-## Wilson Deng
-## Feb 8th, 2019
-## `cov_mx` function calculates the covariance matrix of the two
-## variables and automatically deals with the missing value
-
 import numpy as np
 
 def cov_mx(m):
-  '''
-  Covariance measures the extent to which corresponding observations
-  from two sets of ordered variables vary in a direction.
+    '''
+    Covariance measures the extent to which corresponding observations
+    from two sets of ordered variables vary in a direction.
 
-  parameters:
-  -----------
-  m (2D np.array): a matrix that contains one or more than one random variable
+    parameters:
+    -----------
+    matrix (2D np.array): a matrix that contains one or more than one random variable
 
-  Return:
-  -------
-  cov_matrix (2D np.array): a matrix that contains the covariance between random variables in the input matrix
-  '''
-  # check whether the input is valid or not 
-  m = np.asarray(m)
-  if m.ndim > 2:
-      raise ValueError("m has more than 2 dimensions")
-  
-  m = np.array(m, ndmin=2)
-  # return Nan if 
-  if m.shape[0] == 0: 
-      return np.array([]).reshape(0, 0)
+    Return:
+    -------
+    cov_matrix (2D np.array): a matrix that contains the covariance between random variables in the input matrix
+    '''
+    
+    # check whether the input is valid or not 
+    m = np.asarray(m)
+    if m.ndim == 1:
+        raise TypeError("Input should be a 2D array")
+    elif m.ndim > 2:
+        raise TypeError("m has more than 2 dimensions")
+    
+    m = np.array(m, ndmin=2)
+    if m.size == 2:
+        return None
 
-  return
+    # remove inf and nan values
+    mask = np.any(np.isnan(m) | np.isinf(m), axis=1)
+    m = m[~mask] 
+    
+    # calculate covariance matrix 
+    X = m.copy()
+    X = X.T
+    N = X.shape[0]
+    X = X - X.mean(axis = 0)
+    C = np.dot(X.T, X.conj()) / (N-1) # degree of freedom  
+    
+    return C
