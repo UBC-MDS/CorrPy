9f0769b34c7aa5aad418087111cdf0010951112d
mru4913 <albertyu4913@gmail.com>
2019-02-13 11:43:07 -0800

    Update documentation for test_file.py

--- CorrPy/test/test_corr_plus.py
+++ CorrPy/test/test_corr_plus.py
@@ -25,24 +25,24 @@ missing_y = [2,3,4,5,np.nan,1,3,4]
 rm_x = [1,2,3,4,6,7]
 rm_y = [2,3,4,5,1,3]
 
-# Return Error if wrong type
 def test_type():
+    '''test if the input vectors are valid'''
     with pytest.raises(TypeError):
         CorrPy.corr_plus(mix_type_x,mix_type_y) # expect ERROR if the input are not numeric
         CorrPy.corr_plus(mix_type_x,multi_y_plus) # expect ERROR if the length of two vectors are different
 
-# The output length should be 1
-def test_length():
+def test_output():
+    '''test if the output is returned in the expected format'''
     assert CorrPy.corr_plus(single_x, single_y) == None # expect None if the length of both vectors are one
     assert isinstance(CorrPy.corr_plus(multi_x,mix_type_y), (float, int, complex)) # output should be a number
 
-# Return Error if one of the input is zero
 def test_zero():
+    '''test if one of the input is zero and return ERROR'''
     with pytest.raises(TypeError):
         CorrPy.corr_plus(zeros_x, pos_neg_y) # fail if the input is zero vector
 
-# Test if it can calculate the right value
 def test_value():
+    '''test the correctness of the output'''
     # deal with negative numbers
     assert round(CorrPy.corr_plus(pos_neg_x, pos_neg_y),5) == round(np.corrcoef(pos_neg_x, pos_neg_y)[0,1],5) 
     # can deal with large numbers
--- CorrPy/test/test_std_plus.py
+++ CorrPy/test/test_std_plus.py
@@ -1,5 +1,4 @@
 # Test for std_plus.py
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath("../../CorrPy"))
@@ -18,21 +17,21 @@ large_x = [1000,-2000,3000]
 multi_x = [1,2,3,4,5,6,7]
 miss_x = [1,2,3,4,5,6,7, np.nan]
 
-# Return Error if input data type is not numeric
 def test_type():
+    '''test if the input is in a valid format'''
     with pytest.raises(TypeError):
         CorrPy.std_plus(char_x) # return ERROR if input vector has a string 
         CorrPy.std_plus(complex_x) # return ERROR if input vector has a complext number
     assert np.isnan(CorrPy.std_plus(bool_x)) == False # expect a return if input vector has bool
     assert np.isnan(CorrPy.std_plus(pos_neg_x)) == False # expect a return if input vector type is numeric
         
-# The output should not be none 
-def test_output_format():
+def test_output():
+    '''test if the output is in a valid format'''
     assert np.isnan(CorrPy.std_plus(multi_x)) == False
     assert np.isnan(CorrPy.std_plus(pos_neg_x)) == False
 
-# Test the computed values of standard deviation
 def test_value():
+    '''test the correctness of the output'''
     # return zero when input has one element
     assert CorrPy.std_plus(one_x) == 0.0 
     # compute standard deviation for input of positive and negative numbers
